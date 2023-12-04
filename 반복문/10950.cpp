f0da8cf0919ab02cae267cac8fb3c67dcd9ef485
dhrjsfud0522 <133459434+dhrjsfud0522@users.noreply.github.com>
2023-12-04 08:06:21 +0000

    반복문

---
+++ 반복문/10950.cpp
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/10950
+
+#include <stdio.h>
+
+int main(){
+    int n, a, b;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
