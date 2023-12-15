cf9aa4dfb8a98485cc707bbd73ca718e7bcc8192
dhrjsfud0522 <133459434+dhrjsfud0522@users.noreply.github.com>
2023-12-15 15:11:38 +0000

    일반 수학 1 끝

--- 8. 일반 수학 1/2869.cpp
+++ 8. 일반 수학 1/2869.cpp
@@ -5,7 +5,7 @@
 int main(){
     int a, b, v, n;
     scanf("%d %d %d", &a, &b, &v);
-    if(a > v){
+    if(a >= v){
         printf("1");
         return 0;
     }
@@ -13,12 +13,13 @@ int main(){
         printf("1");
         return 0;
     }
-    if((v + a) % (a - b) == 0){
-        printf("%d", (v - a) / (a - b) + 1);
+    if((v - a) % (a - b) == 0){
+        n = 1;
     }
     else{
-        n = (v - a) % (a - b);
-        printf("%d", (v - a - n) / (a - b) + 2);
+        v -= (v - a) % (a - b);
+        n = 2;
     }
+    printf("%d", (v - a) / (a - b) + n);
     return 0;
 }
